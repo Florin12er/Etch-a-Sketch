16290525be78c74251aced9745fc993e23c5779b
Florin12er <Sebastianflorin603@gmail.com>
2023-09-30 08:01:02 +0200

    Add the design of the Etch-a-Sketch

---
+++ Etch-a-Sketch.js
@@ -0,0 +1,26 @@
+const Black = document.getElementById("Black");
+const Rainbow = document.getElementById("Rainbow");
+const Clear = document.getElementById("Clear");
+const Eraser = document.getElementById("Eraser");
+const changeSize = document.getElementById("Change");
+const Div = document.getElementById("div")
+
+function createDiv() {
+
+};
+function colorRainbow() {
+
+};
+function clearDiv() {
+
+};
+function eraseColor() {
+
+};
+function Size() {
+
+};
+function colorBlack() {
+
+};
+
